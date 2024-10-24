ce542b27d15325708226c47f55d00f967b4bc6b7
itayG98 <itai.g98@gmail.com>
2024-10-24 22:35:03 +0300

    inital work done

--- adjacency/adjacency.c
+++ adjacency/adjacency.c
@@ -1,10 +1,46 @@
 #include <stdio.h>
+#define N 10
+
+typedef enum
+{
+    FALSE = 0,
+    TRUE = 1
+} BOOLEAN;
+
+typedef struct
+{
+    int array[N][N];
+} Adjmant;
+
+void print_matrix(Adjmant arr);
 
 int main()
 {
-    char a = 'a';
-    printf("%c", a);
-    scanf("%c", &a);
-
+    BOOLEAN b = 1;
+    Adjmant a;
+    int i = 0, j = 0;
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            a.array[i][j] = i * j;
+        }
+    }
+    print_matrix(a);
+    scanf("%d", &b);
     return 1;
+}
+
+void print_matrix(Adjmant arr)
+{
+    int i = 0, j = 0;
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            printf("%2d ", arr.array[i][j]);
+        }
+        printf("\n");
+    }
 }
